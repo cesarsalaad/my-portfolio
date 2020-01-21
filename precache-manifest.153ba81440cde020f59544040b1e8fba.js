2cb7556452344055af9499356f716bfd7d498575
Cesar Tesen <clutesen@gmail.com>
2020-01-21 08:59:01 -0800

    Updates

---
+++ precache-manifest.153ba81440cde020f59544040b1e8fba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df10805b53da34a079be185212ad173a",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "8d7777656f171a1bedfe",
+    "url": "/my-portfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "21a01e3268e7d8420821",
+    "url": "/my-portfolio/static/css/main.b48ac709.chunk.css"
+  },
+  {
+    "revision": "8d7777656f171a1bedfe",
+    "url": "/my-portfolio/static/js/2.1cee0b65.chunk.js"
+  },
+  {
+    "revision": "9fdc6469bae9d84fb144a03feb51567e",
+    "url": "/my-portfolio/static/js/2.1cee0b65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21a01e3268e7d8420821",
+    "url": "/my-portfolio/static/js/main.dad65db1.chunk.js"
+  },
+  {
+    "revision": "4184a7eacb7c48bd5c06",
+    "url": "/my-portfolio/static/js/runtime-main.4a96037b.js"
+  }
+]);
