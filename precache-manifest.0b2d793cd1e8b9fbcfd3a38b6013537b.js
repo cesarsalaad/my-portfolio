656e34fb6c1a3d241f3f0c534ed168165f32a0cc
Cesar Tesen <clutesen@gmail.com>
2020-01-28 12:00:44 -0800

    Updates

--- precache-manifest.0b2d793cd1e8b9fbcfd3a38b6013537b.js
+++ precache-manifest.0b2d793cd1e8b9fbcfd3a38b6013537b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0fc53534b1833a84b99c72a6945dcf09",
+    "revision": "da75a7cfeebbbd27b32f3cc55aa2f9a3",
     "url": "/my-portfolio/index.html"
   },
   {
-    "revision": "f1ddec6ef56f738f31c8",
+    "revision": "5137edbdaf2e06c9c7de",
     "url": "/my-portfolio/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "24ea2bd067dcd0874482",
+    "revision": "c565a732cb1c4f40abc9",
     "url": "/my-portfolio/static/css/main.b48ac709.chunk.css"
   },
   {
-    "revision": "f1ddec6ef56f738f31c8",
-    "url": "/my-portfolio/static/js/2.ca7e7883.chunk.js"
+    "revision": "5137edbdaf2e06c9c7de",
+    "url": "/my-portfolio/static/js/2.24c89be7.chunk.js"
   },
   {
     "revision": "5a7a0c9a5ab1cf4469acd111a5848447",
-    "url": "/my-portfolio/static/js/2.ca7e7883.chunk.js.LICENSE"
+    "url": "/my-portfolio/static/js/2.24c89be7.chunk.js.LICENSE"
   },
   {
-    "revision": "24ea2bd067dcd0874482",
-    "url": "/my-portfolio/static/js/main.406fa180.chunk.js"
+    "revision": "c565a732cb1c4f40abc9",
+    "url": "/my-portfolio/static/js/main.fd4c8b66.chunk.js"
   },
   {
     "revision": "4184a7eacb7c48bd5c06",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.c25a47822640013f7e22cb583ae6066e.js"
+  "/my-portfolio/precache-manifest.0b2d793cd1e8b9fbcfd3a38b6013537b.js"
 );
 
 self.addEventListener('message', (event) => {
